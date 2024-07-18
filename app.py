25102e1568b3695d4030440920a01d7954329383
VoiceOfDarkness <abil.samedov2016@yandex.ru>
2024-07-18 22:10:24 +0400

    fix!: maked app variable global

--- app.py
+++ app.py
@@ -6,19 +6,13 @@ from host.host import host_bp
 from player.player import player_bp
 from database import mongo
 
+app = Flask(__name__, static_folder="static", template_folder="templates")
+app.config.from_object(Config)
+mongo.init_app(app)
 
-def create_app():
-    app = Flask(__name__, static_folder="static", template_folder="templates")
-    app.config.from_object(Config)
-    mongo.init_app(app)
-
-    app.register_blueprint(home_bp)
-    app.register_blueprint(host_bp)
-    app.register_blueprint(player_bp)
-
-    return app
-
+app.register_blueprint(home_bp)
+app.register_blueprint(host_bp)
+app.register_blueprint(player_bp)
 
 if __name__ == "__main__":
-    app = create_app()
     app.run()
